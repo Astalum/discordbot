c1e85009332a6cc840f2d3c9a6216f3ce0e2c76d
Asutalum <taaro20040526@icloud.com>
2024-03-13 07:23:02 +0900

    main.pyをコピペ

---
+++ main.py
@@ -0,0 +1,27 @@
+import discord
+import config
+
+intents = discord.Intents.all()
+client=discord.client(intents=intents)
+
+# bot起動時に発火
+@client.Event
+async def on_ready():
+    print("Ready!")
+
+# メッセージの検知
+@client.event
+async def on_message(message):
+    # 自身が送信したメッセージには反応しない
+    if message.author == client.user:
+        return
+
+    # ユーザーからのメンションを受け取った場合、あらかじめ用意された配列からランダムに返信を返す
+    if client.user in message.mentions:
+
+        answer = "Hello World!"
+        print(answer)
+        await message.channel.send(answer)
+
+# Bot起動
+client.run(config.DISCORD_TOKEN)
